3f5251ced92e01b3082e694c687ed0353890d312
akana3866 <akanaujia8@gmail.com>
2024-09-26 12:27:50 -0400

    Edited some more CSS
    trying to also edit image

--- front-end/src/AboutUs.js
+++ front-end/src/AboutUs.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import image from './aditi_abt.jpg' //not used just there for testing
 import axios from 'axios';
 
 /**
@@ -18,7 +19,7 @@ const AboutUs = props => {
         setData(response.data);
         setisLoaded(true);
       } catch (err) {
-        setError('Error loading About Us content.');
+        setError('Error loading content.');
         console.error(err);
       }
     };
@@ -36,11 +37,13 @@ const AboutUs = props => {
 
   return (
     <div className="AboutUs">
-      <h1>{abtInfo ? abtInfo.title : 'Sorry this is taking a while.'}</h1>
-      <p>{abtInfo ? abtInfo.description : 'Please wait, loading some cool stuff about me :)'}</p>
-      <img src={abtInfo ? abtInfo.imageURL : '#'} alt="About Us" className="about-img" />
+      <h1 style={{ marginBottom: '20px' }}>{abtInfo ? abtInfo.title : 'Sorry this is taking a while.'}</h1>
+      <p style={{ marginBottom: '16px', textAlign: 'left' }}>{abtInfo ? abtInfo.description : 'Please wait, loading some cool stuff about me :)'}</p>
+      <p style={{ marginBottom: '20px', textAlign: 'left' }}>{abtInfo ? abtInfo.description2 : 'Please wait, loading some more cool stuff about me :)'}</p>
+      <img src={abtInfo ? abtInfo.imageURL : '#'} alt="About Us" style={{ display: 'block', marginTop: '20px', marginLeft: 'auto', marginRight: 'auto' }} className="about-img" />
     </div>
   );
+  
 };
 
 export default AboutUs;
